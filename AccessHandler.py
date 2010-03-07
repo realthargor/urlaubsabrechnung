5717c6a9a20447ba46d948cc4cc5cdfb661c1bdc
danielbelucha <danielbelucha@7a212eb2-2389-11df-aa88-49b14aa33a48>
2010-03-07 21:23:32 +0000

    Class based methods completed -- somehow working

--- AccessHandler.py
+++ AccessHandler.py
@@ -22,10 +22,10 @@ from google.appengine.api import mail
 						
 class AccessHandler(BaseRequestHandler):
 	def	post(self):
-		(project, right)=self.getandcheckproject(1)
+		self.updateproject()
 		action = self.request.get('action','list')
 		if action=='invite':	
-			i = Invitation(project=project, user=users.User(email=self.request.get('email')), right=int(self.request.get('right')), code=random.randint(0, 2<<31))
+			i = Invitation(project=self.project, user=users.User(email=self.request.get('email')), right=int(self.request.get('right')), code=random.randint(0, 2<<31))
 			i.put()
 			message = mail.EmailMessage(sender=users.get_current_user().email(),
 										subject="Invitation to Urlaubsabrechnung")
@@ -33,9 +33,9 @@ class AccessHandler(BaseRequestHandler):
 			message.body = "Hallo!\n\nDu bist eingeladen die Abrechnung fuer %(projectname)s anzusehen.\n\n" \
 			"Bitte melde dich unter\nhttp://urlaubsabrechnung.appspot.com/join?project=%(project)s&invitation=%(invitation)s&code=%(code)d"\
 			"\nmit einem google account an.!" % {
-													'projectname':  project.name,
+													'projectname':  self.project.name,
 													'invitation':  i.key(),
-													'project': project.key(),
+													'project': self.project.key(),
 													'code': i.code
 												}
 			message.send()
@@ -46,25 +46,8 @@ class AccessHandler(BaseRequestHandler):
 			raise Exception("Unknown action '%(action)s'!" % {'action':action})
 			
 	def	get(self):
-		(project, right)=self.getandcheckproject(1)
+		self.updateproject()
 		self.generate('access', {
-			'project': project,
-			'project_key': project.key(),
-			'rights': [
-				{
-					'user': right.user,
-					'right': right.right,
-					'key': right.key(),
-				} for right in project.projectrights_set
-			],
-			'invitations': [
-				{
-					'user': invitation.user,
-					'right': invitation.right,
-					'created': invitation.created,
-					'invitation': invitation.key(),
-					'code': invitation.code,
-					'project': invitation.project.key(),
-				} for invitation in project.invitation_set
-			]
-		})
+				'rights': self.project.projectrights_set,
+				'invitations': self.project.invitation_set,
+			})
--- AccountsHandler.py
+++ AccountsHandler.py
@@ -21,9 +21,9 @@ from django.utils import simplejson as json
 from google.appengine.api import mail
 						
 class AccountsHandler(BaseRequestHandler):
-	def listGroups(self, project, type, default):
+	def listGroups(self, type, default=None):
 		# list remaining groups
-		for a in project.person_set:
+		for a in self.project.person_set:
 			if isinstance(a, type):
 				if default==None or default.key()==a.key():
 					self.response.out.write('<option selected value="%(value)s">%(name)s</option>' % {'name':a.name, 'value':a.key()})
@@ -32,7 +32,7 @@ class AccountsHandler(BaseRequestHandler):
 					self.response.out.write('<option value="%(value)s">%(name)s</option>' % {'name':a.name, 'value':a.key()})
 		
 	def	post(self):
-		(project, right)=self.getandcheckproject(1)
+		self.updateproject()
 		action = self.request.get('action','list')		 
 		if action=='list':
 			# select the class type we want to list
@@ -44,7 +44,7 @@ class AccountsHandler(BaseRequestHandler):
 			else:
 				target_class=object
 			# create a list of options against all accounts
-			self.listGroups(project, target_class, None)
+			self.listGroups(type=target_class)
 		
 		elif action=='member_remove':
 			# remove memberships
@@ -71,7 +71,7 @@ class AccountsHandler(BaseRequestHandler):
 					m.put()
 			# add new memberships for all remaining items
 			for a in update_account_list:
-				Member(project=project, group=group, weight=member_weight, person=Person.get(a)).put()
+				Member(project=self.project, group=group, weight=member_weight, person=Person.get(a)).put()
 			# delete all members with weight 0
 			for member in Group.get(self.request.get('group')).member_set:
 				if member.weight == 0:
@@ -83,65 +83,66 @@ class AccountsHandler(BaseRequestHandler):
 		elif action=='group_add':
 			# add a new group
 			newname = self.request.get('name')
-			project.CheckNewAccountName(newname)
-			group = Group(name=newname, project=project)
+			self.project.CheckNewAccountName(newname)
+			group = Group(name=newname, project=self.project)
 			group.put()
 			# list new remaining groups
-			self.listGroups(project, Group, group)
+			self.listGroups(type=Group, default=group)
 			
 		elif action=='person_add':
 			# add a new person
 			newname = self.request.get('name')
-			project.CheckNewAccountName(newname)
-			person = Person(name=newname, project=project)
+			self.project.CheckNewAccountName(newname)
+			person = Person(name=newname, project=self.project)
 			person.put()
 			# list the persons
-			self.listGroups(project, Person, person)
+			self.listGroups(type=Person, default=person)
 			
 		elif action=='person_remove':
 			# remove a list of persons
 			for person_key in json.loads(self.request.get('person_list')):
 				Account.get(person_key).delete()
 			# list persons
-			self.listGroups(project, Person, None)
+			self.listGroups(type=Person)
 		
 		elif action=='person_rename':
 			# rename an account
 			account = Account.get(self.request.get('account'));
 			newname = self.request.get('name', account.name)
-			project.CheckNewAccountName(newname, account)
+			self.project.CheckNewAccountName(newname, account)
 			account.name = newname
 			account.put()
 			# list accounts
-			self.listGroups(project, Person, account)
+			self.listGroups(type=Person, default=account)
 
 		elif action=='group_rename':
 			# rename an account
 			account = Account.get(self.request.get('account'));
 			newname = self.request.get('name', account.name)
-			project.CheckNewAccountName(newname, account)
+			self.project.CheckNewAccountName(newname, account)
 			account.name = newname
 			account.put()
 			# list accounts
-			self.listGroups(project, Group, account)		
+			self.listGroups(type=Group, default=account)		
 			
 		elif action=='group_remove':
 			# remove a group
 			Group.get(self.request.get('group')).delete();
 			# list remaining groups
-			self.listGroups(project, Group, None)
+			self.listGroups(type=Group)
 			
 		elif action=='members':
 			# list members of a group
 			for member in Group.get(self.request.get('group')).member_set:
-				self.response.out.write('<option value="%(value)s">%(name)s: %(weight)f</option>' % {'name':member.person.name, 'value':member.key(), 'weight': member.weight })
+				self.response.out.write('<option value="%(value)s">%(name)s: %(weight)f</option>' % {
+					'name':member.person.name, 
+					'value':member.key(), 
+					'weight': member.weight 
+				})
 		
 		else:
 			raise Exception("Unknown action '%(action)s'!" % {'action':action})
 
 	def	get(self):
-		(project, right)=self.getandcheckproject(1)
-		self.generate('accounts', {
-			'project': project,
-			'project_key': project.key(),
-		})
+		self.updateproject()
+		self.generate('accounts')
