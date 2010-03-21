58efa064dd62d9e8542751936bc2c456fdcf2584
danielbelucha <danielbelucha@7a212eb2-2389-11df-aa88-49b14aa33a48>
2010-03-21 09:21:22 +0000

    Integration into Eclipse started

--- JoinHandler.py
+++ JoinHandler.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python
-import os
-import cgi
-import datetime
-import wsgiref.handlers
-import StringIO
-import random
 
-from models import *
-from BaseRequestHandler import BaseRequestHandler
-
-from google.appengine.ext import db
+from models import ProjectRights, Invitation
 from google.appengine.api import users
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template 
-from google.appengine.ext.db import polymodel
 from google.appengine.ext.db import BadKeyError
 from google.appengine.ext.webapp.util import login_required
-from datetime import datetime
-from django.utils import simplejson as json
-from google.appengine.api import mail
 						
 class JoinHandler(webapp.RequestHandler):
 	@login_required
@@ -28,9 +14,9 @@ class JoinHandler(webapp.RequestHandler):
 			if i is None:
 				self.redirect("/")
 				return
-			# only allow privilige increase, when the project and the code matches the stored Invitation instance
+			# only allow privilege increase, when the project and the code matches the stored Invitation instance
 			if int(self.request.get('code', 0))==i.code and str(self.request.get('project', ''))==str(i.project.key()):
-				# check for existiting access to project
+				# check for existing access to project
 				rights = ProjectRights.gql("WHERE user=:user and project=:project", user=users.get_current_user(), project=i.project).get()
 				# edit rights
 				if not rights:
@@ -44,10 +30,9 @@ class JoinHandler(webapp.RequestHandler):
 			else:
 				# invalid invitation
 				raise Exception("Invalid invitation %(p1)s!=%(p2)s" % {'p1': self.request.get('project', ''), 'p2':i.project.key() } )
-			# redirect to project summary, even if we can not increase priveliges
+			# redirect to project summary, even if we can not increase privilege
 			self.redirect("/summary?project=%(project)s" % { 'project': i.project.key() } )
 		except BadKeyError:
 			# 
 			self.redirect("/summary?project=%(project)s" % { 'project': self.request.get('project', 'invalid')} )
 			return 
- 
--- models.py
+++ models.py
@@ -2,18 +2,12 @@
 # ******************************************************************************
 # ** DATA MODELS ***************************************************************
 # ******************************************************************************
-import os
-import cgi
-import datetime
 import StringIO
 
 from google.appengine.ext import db
 from google.appengine.api import users
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template 
 from google.appengine.ext.db import polymodel
 from google.appengine.ext.db import BadKeyError
-from google.appengine.ext.webapp.util import login_required
 
 class Project(db.Model):
 	name = db.StringProperty()
@@ -31,17 +25,17 @@ class Project(db.Model):
 	""" returns all list of all projects the user actually has rights for """
 	@staticmethod
 	def all():
-		projects=()
+		projects = ()
 		for right in ProjectRights.gql("WHERE user=:user AND right>0", user=users.get_current_user()):
-			right.project.rights =  right.right			
+			right.project.rights = right.right			
 			projects.append(right.project)
 		return projects				
 		
 	def RightReport(self):
-		return self.rights&1
+		return self.rights & 1
 		
 	def RightTransaction(self):
-		return self.rights & (2|4|8)
+		return self.rights & (2 | 4 | 8)
 		
 	def RightReportView(self):
 		return self.rights & 1
@@ -88,17 +82,17 @@ class Project(db.Model):
 		
 	""" checks wether the given name is a valid new name for a a new account or a given existing account """
 	def CheckNewAccountName(self, name, account=None):
-		if len(name)<1:
+		if len(name) < 1:
 			raise Exception("account name must be at least on character long")
 		for a in self.person_set:
-			if a.name==name and (account==None or a.key()!=account.key()):
+			if a.name == name and (account == None or a.key() != account.key()):
 				raise Exception("duplicate name")
 				
 	""" Helper to format a single value """
 	def formatTableValue(self, value):
-		if value<0:
+		if value < 0:
 			return '<td style="color: red; text-align:right;">%(v).2f%(unit)s</td>' % {'v':-value, 'unit': self.currency }
-		elif value>0:
+		elif value > 0:
 			return '<td style="color: black; text-align:right;">%(v).2f%(unit)s</td>' % {'v':value, 'unit': self.currency}
 		else:
 			return "<td/>"
@@ -125,16 +119,16 @@ class Project(db.Model):
 		res = StringIO.StringIO()
 		res.write('<ul>')
 		for currency in self.currency_set:			
-			if currency.divisor==1 :
+			if currency.divisor == 1 :
 				res.write('<li>%(divisor)f %(name)s = %(factor)f %(base)s</li>' % {
-					'name': currency.name,	
+					'name': currency.name, 	
 					'factor': currency.factor,
 					'divisor': currency.divisor,
 					'base': self.currency
 				})				
 			else:
 				res.write('<li>%(factor)f %(base)s = %(divisor)f %(name)s</li>' % {
-					'name': currency.name,	
+					'name': currency.name, 	
 					'factor': currency.factor,
 					'divisor': currency.divisor,
 					'base': self.currency
@@ -147,23 +141,24 @@ class Project(db.Model):
 		# TRANSACTIONS
 		res = dict()
 		endpoints = self.Endpoints()
-		res['endpoints']=endpoints
+		res['endpoints'] = endpoints
 		sums = dict()
-		tr=[];
-		for transaction in sorted(self.transaction_set, cmp=lambda x,y: cmp(x.date, y.date)):
+		tr = [];
+		for transaction in sorted(self.transaction_set, cmp=lambda x, y: cmp(x.date, y.date)):
 			affected = transaction.UpdateSums(balance=dict())
-			transaction.affected = [affected.get(endpoint.key(),0) for endpoint in endpoints]
-			for (key,value) in affected.iteritems():
-				sums[key]=sums.get(key,0)+value
+			transaction.affected = [affected.get(endpoint.key(), 0) for endpoint in endpoints]
+			for (key, value) in affected.iteritems():
+				sums[key] = sums.get(key, 0) + value
 			tr.append(transaction)
 		res['transactions'] = tr
-		res['sums']= [ round(sum,2) for sum in sums.itervalues() ]
+		res['sums'] = [ round(sum, 2) for sum in sums.itervalues() ]
 		return res
-  
+
+
 class Account(polymodel.PolyModel):
 	name = db.StringProperty(required=True)
 	def IsEndpoint(self):
-		return isinstance(self, Person) or sum([abs(member.weight) for member in self.member_set])==0	
+		return isinstance(self, Person) or sum([abs(member.weight) for member in self.member_set]) == 0	
 		
 	""" return the sum of weights for the given group """
 	def SumOfMemberWeigths(self):
@@ -176,24 +171,24 @@ class Account(polymodel.PolyModel):
 		# calculate the sum of weights
 		sweight = self.SumOfMemberWeigths()
 		# when the weight sum is zero, that means, there are no valid members, thus we map that to the account
-		if sweight==0:
-			balance[self.key()]=balance.get(self.key(),0)+sign*amount;
+		if sweight == 0:
+			balance[self.key()] = balance.get(self.key(), 0) + sign * amount;
 			return balance
 		# now we create a dictionary
 		ramount = amount		# remaing amount
 		rweight = sweight		# remaing sum of weights
 		# walk through the sorted list, the last member (the one with the largest weight) gets the remaining sum
-		for member in sorted(self.member_set, cmp=lambda x,y: cmp(x.weight, y.weight)):
-			rweight-=abs(member.weight)
-			val = round(amount*abs(member.weight)/sweight, 2) if rweight>0 else ramount
-			ramount-=val
-			balance[member.person.key()]=balance.get(member.person.key(),0)+sign*val
+		for member in sorted(self.member_set, cmp=lambda x, y: cmp(x.weight, y.weight)):
+			rweight -= abs(member.weight)
+			val = round(amount * abs(member.weight) / sweight, 2) if rweight > 0 else ramount
+			ramount -= val
+			balance[member.person.key()] = balance.get(member.person.key(), 0) + sign * val
 		# return dictionary again
 		return balance
 		
 	""" returns the number of transactions affected by deleting this Account """
 	def ByDeleteAffectedTransactions(self):
-		return len(self.accountmodel_reference_source_set)+len(self.accountmodel_reference_dest_set)
+		return len(self.accountmodel_reference_source_set) + len(self.accountmodel_reference_dest_set)
 		
 	""" overwrites polymodel.PolyModel.delete() and also deletes all references """
 	def delete(self):
@@ -215,7 +210,7 @@ class Person(Account):
 			membership.delete()
 		# call base method (which actually deletes this instance from the data store)
 		Account.delete(self)
-  
+
 class Group(Account):
 	project = db.ReferenceProperty(Project)
 	
@@ -227,18 +222,18 @@ class Group(Account):
 		# call base method (which actually deletes this instance from the data store)
 		Account.delete(self)
 	
-  
+
 class Member(db.Model):
-  group = db.ReferenceProperty(Group)
-  person = db.ReferenceProperty(Person)
-  weight = db.FloatProperty()	
-    
+	group = db.ReferenceProperty(Group)
+	person = db.ReferenceProperty(Person)
+	weight = db.FloatProperty()	
+
 class Currency(db.Model):
-  project = db.ReferenceProperty(Project)
-  name = db.StringProperty()
-  factor = db.FloatProperty(default=1.0)
-  divisor = db.FloatProperty(default=1.0)
-   
+	project = db.ReferenceProperty(Project)
+	name = db.StringProperty()
+	factor = db.FloatProperty(default=1.0)
+	divisor = db.FloatProperty(default=1.0)
+
 class Transaction(db.Model):
 	project = db.ReferenceProperty(Project)
 	source = db.ReferenceProperty(Account, collection_name="accountmodel_reference_source_set")
@@ -253,22 +248,22 @@ class Transaction(db.Model):
 	
 	""" returns the amount of the transaction using in the project currency """
 	def AmountBase(self):
-		return self.ammount*self.currency.factor/self.currency.divisor if self.currency else self.ammount
+		return self.ammount * self.currency.factor / self.currency.divisor if self.currency else self.ammount
 	
 	""" returns the currency text """
 	def CurrencyName(self):
 		return self.currency.name if self.currency else self.project.currency
 			
 	def UpdateSums(self, balance=dict()):
-		self.dest.enquire(amount=self.AmountBase(),balance=balance, sign=-1)
-		self.source.enquire(amount=self.AmountBase(),balance=balance, sign=+1)
+		self.dest.enquire(amount=self.AmountBase(), balance=balance, sign= -1)
+		self.source.enquire(amount=self.AmountBase(), balance=balance, sign= +1)
 		return balance
 
 class ProjectRights(db.Model):
-  project = db.ReferenceProperty(Project, required=True)
-  user = db.UserProperty(required=True)
-  right = db.IntegerProperty()
-  
+	project = db.ReferenceProperty(Project, required=True) #@IndentOk
+	user = db.UserProperty(required=True)
+	right = db.IntegerProperty()
+	
 """
 	Invitation represents a not yet turned in access key to a project with the given rights
 	The invited person receives an email together with the key of the invitation and a random
@@ -277,9 +272,9 @@ class ProjectRights(db.Model):
 	is deleted.
 """
 class Invitation(db.Model):
-  project = db.ReferenceProperty(Project, required=True)
-  invited_by = db.UserProperty(auto_current_user=True)
-  user = db.UserProperty(required=True)
-  right = db.IntegerProperty(required=True)
-  code = db.IntegerProperty(required=True)
-  created = db.TimeProperty(auto_now_add=True)  
+	project = db.ReferenceProperty(Project, required=True)
+	invited_by = db.UserProperty(auto_current_user=True)
+	user = db.UserProperty(required=True)
+	right = db.IntegerProperty(required=True)
+	code = db.IntegerProperty(required=True)
+	created = db.TimeProperty(auto_now_add=True)  
